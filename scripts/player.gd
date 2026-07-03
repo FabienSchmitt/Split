9400153d6d58656081469bbd4c4e8137692a6692
pascal.heritier <pascalheritier>
2026-07-04 00:49:07 +0200

    Sprite changes when moving

--- scripts/player.gd
+++ scripts/player.gd
@@ -27,29 +27,34 @@ func attack() -> void:
 	current_spit = spit
 
 func _physics_process(delta: float) -> void:
-	
 	if Input.is_action_just_pressed("attack"):
 		attack()
 
-	# Get the input direction and handle the movement/deceleration.
-	# Can be -1, 0, 1
 	var current_direction := Input.get_axis("move_left", "move_right")
 	if current_direction != 0 && current_direction != player_facing_direction:
 		change_direction(current_direction)
-	
+
 	if ray_cast_2d_right.is_colliding():
-		if( current_direction > 0):
+		if current_direction > 0:
 			current_direction = 0
 	if ray_cast_2d_left.is_colliding():
-		if( current_direction < 0):
+		if current_direction < 0:
 			current_direction = 0
 
 	if current_direction:
 		velocity.x = current_direction * SPEED
+		update_animation(current_direction)
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		animated_sprite_2d.play("idle")
 
 	move_and_slide()
+
+func update_animation(direction: float) -> void:
+	if direction < 0:
+		animated_sprite_2d.play("left_move")
+	elif direction > 0:
+		animated_sprite_2d.play("right_move")
 
 func change_direction(direction: float) -> void:
 	player_facing_direction = direction
