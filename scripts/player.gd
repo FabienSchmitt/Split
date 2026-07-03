a992a4dc6571b7b68f59deb4f6bf27826683d929
pascal.heritier <pascalheritier>
2026-07-03 22:37:30 +0200

    Add boundaries

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,5 @@
-extends Area2D
-
-const SPEED = 60;
+extends CharacterBody2D
+const SPEED = 450;
 
 @onready var ray_cast_2d_right: RayCast2D = $RayCast2D_Right
 @onready var ray_cast_2d_left: RayCast2D = $RayCast2D_Left
@@ -9,11 +8,23 @@ const SPEED = 60;
 
 var direction = 1;
 
-func _process(delta):
+func _physics_process(delta: float) -> void:
+	# Get the input direction and handle the movement/deceleration.
+	# Can be -1, 0, 1
+	var current_direction := Input.get_axis("move_left", "move_right")
+	
 	if ray_cast_2d_right.is_colliding():
-		direction = -1;
+		if( current_direction > 0):
+			current_direction = 0
 		animated_sprite_2d.flip_h = true;
 	if ray_cast_2d_left.is_colliding():
-		direction = 1;
+		if( current_direction < 0):
+			current_direction = 0
 		animated_sprite_2d.flip_h = false;
-	position.x += direction * SPEED * delta;
+
+	if current_direction:
+		velocity.x = current_direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
