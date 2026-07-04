a113edd5a080aef8d6cac142da785774df7634a6
Fabien Schmitt <fab.schmitt@hotmail.com>
2026-07-04 11:04:01 +0200

    renaming player and adding multiplayer

--- scripts/lama.gd
+++ scripts/lama.gd
@@ -1,37 +1,44 @@
+class_name Lama
 extends CharacterBody2D
 const SPEED = 450;
 
 @onready var ray_cast_2d_right: RayCast2D = $RayCast2D_Right
 @onready var ray_cast_2d_left: RayCast2D = $RayCast2D_Left
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var is_multiplayer := false
 
 @export var spit_scene: PackedScene
 
 var current_spit = null
-# Can be -1 (left) or 1 (right)
-var player_facing_direction: float = 1
+var current_direction := 1.0
+var _multiplayer_handler: MultiplayerHandler
+var _singleplayer_handler: SingleplayerHandler
 
-func attack() -> void:
+# Can be -1 (left) or 1 (right)
+var lama_facing_direction: float = 1
+
+func _ready() -> void:
+	_multiplayer_handler = MultiplayerHandler.new(self)
+	_singleplayer_handler = SingleplayerHandler.new(self)
+
+
+func attack(spit_direction: Vector2) -> void:
 	var spit = spit_scene.instantiate()
 	get_tree().current_scene.add_child(spit)
 	spit.global_position = global_position
 	spit.global_position.y -= 10 #to center it with the player sprite
-
-	var input_vector := Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	if input_vector == Vector2.ZERO:
-		input_vector = Vector2(player_facing_direction, 0)
-	else:
-		input_vector = input_vector.normalized()
-
-	spit.set_direction(input_vector)
+	spit.set_direction(spit_direction)
 	current_spit = spit
 
 func _physics_process(delta: float) -> void:
-	if Input.is_action_just_pressed("attack"):
-		attack()
+	
 
-	var current_direction := Input.get_axis("move_left", "move_right")
-	if current_direction != 0 && current_direction != player_facing_direction:
+	if is_multiplayer:
+		_multiplayer_handler.handle_inputs()
+	else: 
+		_singleplayer_handler.handle_input()
+
+	if current_direction != 0 && current_direction != lama_facing_direction:
 		change_direction(current_direction)
 
 	if ray_cast_2d_right.is_colliding():
@@ -57,5 +64,5 @@ func update_animation(direction: float) -> void:
 		animated_sprite_2d.play("right_move")
 
 func change_direction(direction: float) -> void:
-	player_facing_direction = direction
+	lama_facing_direction = direction
 	animated_sprite_2d.flip_h = false
