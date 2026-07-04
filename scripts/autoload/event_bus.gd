cabc148bbd5b15e5f8955a4f9e22719d61a3198f
pascal.heritier <pascalheritier>
2026-07-04 10:19:43 +0200

    Add life to HUD

--- scripts/autoload/event_bus.gd
+++ scripts/autoload/event_bus.gd
@@ -3,4 +3,7 @@ extends Node
 @warning_ignore("unused_signal")
 signal player_score_added(scoreToAdd: int)
 
+@warning_ignore("unused_signal")
+signal player_life_lost(lifeToLose: int)
+
 # signal tourist_has_been_hit(tourist: Tourist)
--- scripts/player_hud.gd
+++ scripts/player_hud.gd
@@ -1,12 +1,21 @@
 extends CanvasLayer
 
-func _ready() -> void:
-	EventBus.player_score_added.connect(add_score)
-
 @onready var score_label: Label = $MarginContainer/PanelContainer/MarginContainer/GridContainer/Score
+@onready var life_label: Label = $MarginContainer2/PanelContainer/MarginContainer/GridContainer/Life
 
 var score = 0
+var life = 10
+
+func _ready() -> void:
+	score_label.text = "Score: " + str(score)
+	life_label.text = "Life: " + str(life)
+	EventBus.player_score_added.connect(add_score)
+	EventBus.player_life_lost.connect(lose_life)
 
 func add_score(scoreToAdd: int) -> void:
 	score += scoreToAdd
 	score_label.text = "Score: " + str(score)
+
+func lose_life(lifeToLose: int) -> void:
+	life -= lifeToLose
+	life_label.text = "Life: " + str(life)
