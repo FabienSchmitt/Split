94e605f9ebea64ee654f3d4564a7c66460f98481
pascal.heritier <pascalheritier>
2026-07-04 20:31:19 +0200

    Level1 completed

--- scripts/level_1.gd
+++ scripts/level_1.gd
@@ -1,12 +1,16 @@
 extends Node2D
 
-@onready var TSP_Bottom_Left: Node2D = $Tourists/Bottom_left/TouristSpawnPoint
-@onready var TSP_Up_Center: Node2D = $Tourists/Up_center/TouristSpawnPoint
-@onready var TSP_Up_Right: Node2D = $Tourists/Up_right/TouristSpawnPoint
+@onready var TSP_Left_bottom: Node2D = $Tourists/Left_bottom/TouristSpawnPoint
+@onready var TSP_Left_up: Node2D = $Tourists/Left_up/TouristSpawnPoint
+@onready var TSP_Right_bottom: Node2D = $Tourists/Right_bottom/TouristSpawnPoint
+@onready var TSP_Right_up: Node2D = $Tourists/Right_up/TouristSpawnPoint
+@onready var TSP_Center_up: Node2D = $Tourists/Center_up/TouristSpawnPoint
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	# create timers to enable the TSP after a delay
-	get_tree().create_timer(3.0).timeout.connect(func (): TSP_Bottom_Left.enable_spawning())
-	get_tree().create_timer(8.7).timeout.connect(func (): TSP_Up_Center.enable_spawning())
-	get_tree().create_timer(12.0).timeout.connect(func (): TSP_Up_Right.enable_spawning())
+	get_tree().create_timer(3.0).timeout.connect(func (): TSP_Left_bottom.enable_spawning())
+	get_tree().create_timer(8.7).timeout.connect(func (): TSP_Left_up.enable_spawning())
+	get_tree().create_timer(12.0).timeout.connect(func (): TSP_Right_bottom.enable_spawning())
+	get_tree().create_timer(15.0).timeout.connect(func (): TSP_Right_up.enable_spawning())
+	get_tree().create_timer(18.0).timeout.connect(func (): TSP_Center_up.enable_spawning())
