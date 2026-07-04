1795bb307e549d687649054e72c173d5e375fd33
pascal.heritier <pascalheritier>
2026-07-04 17:08:22 +0200

    Limit max tourists when spawning

--- scripts/gameplay/tourist_spawn_point.gd
+++ scripts/gameplay/tourist_spawn_point.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 @export var tourist_scale: Vector2 = Vector2.ONE
 @export var tourist_speed : float = 20
+@export var tourists_max : int = 5
 
 
 @onready var pop_timer : Timer = %PopTimer
@@ -11,10 +12,24 @@ var tourist_scene : PackedScene
 
 func _ready() -> void:
 	tourist_scene = load("res://scenes/gameplay/tourist.tscn")
+	addTourist()
 	pop_timer.timeout.connect(pop)
 
 func pop() -> void: 
+	if get_tourist_count() >= tourists_max:
+		return
+	addTourist()
+
+
+func addTourist() -> void:
 	var new_tourist : Tourist = tourist_scene.instantiate()
 	new_tourist.speed = tourist_speed
 	new_tourist.scale = tourist_scale
 	self.add_child(new_tourist)
+
+func get_tourist_count() -> int:
+	var count := 0
+	for child in get_children():
+		if child is Tourist:
+			count += 1
+	return count
