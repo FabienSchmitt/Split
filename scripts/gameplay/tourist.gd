9af9b71c10be709ce9e6f40afdd609c8f9b0c219
Fabien Schmitt <fab.schmitt@hotmail.com>
2026-07-04 17:32:01 +0200

    improve tourists

--- scripts/gameplay/tourist.gd
+++ scripts/gameplay/tourist.gd
@@ -30,12 +30,12 @@ var idle_animation : String :
 var speed : float = 50
 var direction : Vector2
 var timer: SceneTreeTimer
+var should_attack = false
 
 func _ready() -> void:
 	direction = Vector2.LEFT if randi_range(0, 1) == 0  else Vector2.RIGHT
 	hit_component.is_hit.connect(_has_been_hit)
 	life_component.died.connect(_die)
-	EventBus.game_is_over.connect(func(): state_machine.change_state(state_game_over))
 	var random_skin = randi_range(1, 4)
 	tourist_number = str(random_skin)
 	
@@ -50,8 +50,9 @@ func _set_state_machine() -> void :
 	
 
 func _set_attack_timer() -> void : 
+	should_attack = false
 	timer = get_tree().create_timer(5.0)
-	timer.timeout.connect(func(): state_machine.change_state(state_photo))
+	timer.timeout.connect(func(): should_attack = true)
 
 
 func _physics_process(delta: float) -> void:
@@ -62,6 +63,13 @@ func enter_state_walk():
 	_set_attack_timer()
 
 func state_walk(): 
+	
+	if GameManager.is_game_over: 
+		state_machine.change_state(state_game_over)
+
+	if should_attack:
+		state_machine.change_state(state_photo)
+	
 	if (direction == Vector2.LEFT && not ray_cast_left.is_colliding()):
 		direction = Vector2.RIGHT
 		sprite.flip_h = false
@@ -74,13 +82,33 @@ func state_walk():
 
 
 func enter_state_photo():
-	attack()
+	sprite.play(photo_animation)
+	await sprite.animation_finished
+ 
+	# at this point, the player will be hit. 
+	if life_component.is_dead or GameManager.is_game_over: 
+		return
+
+	disable_collisions()
+
+	EventBus.player_life_lost.emit(1)
+	var flash = flash_scene.instantiate()
 	
+	get_tree().current_scene.add_child(flash)
+
+	var photo_tween = get_tree().create_tween()
+	photo_tween.tween_property(sprite, "modulate", Color(1, 1, 1, 0), 3.0)
+	await photo_tween.finished
+	
+
 func state_photo():
+	if GameManager.is_game_over: 
+		state_machine.change_state(state_game_over)
 	if sprite.modulate.a == 0:
 		queue_free()
-	
-	
+
+func state_attack():
+	pass	
 
 func enter_state_game_over():
 	sprite.play(idle_animation)
@@ -92,6 +120,11 @@ func enter_state_die():
 	disable_collisions()
 	sprite.play(die_animation)
 	await sprite.animation_finished
+
+	var die_tween = get_tree().create_tween()
+	die_tween.tween_property(sprite, "modulate", Color(1, 1, 1, 0), 1.0)
+	await die_tween.finished
+
 	queue_free()
 
 func state_die():
@@ -109,16 +142,7 @@ func disable_collisions() -> void:
 func _die() -> void:
 	state_machine.change_state(state_die)
 
-func attack() -> void:
-	sprite.play(photo_animation)
-	await sprite.animation_finished
-	EventBus.player_life_lost.emit(1)
-	var flash = flash_scene.instantiate()
-	get_tree().current_scene.add_child(flash)
 
-	var photo_tween = get_tree().create_tween()
-	photo_tween.tween_property(sprite, "modulate.a", 0.0, 1.0)
-	await photo_tween.finished
 
 	
 	
