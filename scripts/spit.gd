8f42e4c0f82a81ca002364e925a03f1bffb5e4c4
pascal.heritier <pascalheritier>
2026-07-04 14:44:31 +0200

    Fix spit impac sound

--- scripts/spit.gd
+++ scripts/spit.gd
@@ -33,5 +33,7 @@ func destroy() -> void:
 func _on_area_entered(area: Area2D) -> void:
 	#if area.get_parent() != null && area.get_parent().is_in_group("tourist"):
 	#	area.get_parent().take_damage(1)
+	visible = false
 	impact_sound_2d.play()
+	await impact_sound_2d.finished
 	destroy()
