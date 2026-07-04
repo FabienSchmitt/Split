9101b535bf2532920e11a971017679bddce07bac
pascal.heritier <pascalheritier>
2026-07-04 17:36:28 +0200

    Add up center tourists

--- scripts/level_1.gd
+++ scripts/level_1.gd
@@ -1,9 +1,10 @@
 extends Node2D
 
 @onready var TSP_Bottom_Left: Node2D = $Tourists/Bottom_left/TouristSpawnPoint
+@onready var TSP_Up_Center: Node2D = $Tourists/Up_center/TouristSpawnPoint
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	# create timers to enable the TSP after a delay
 	get_tree().create_timer(5.0).timeout.connect(func (): TSP_Bottom_Left.enable_spawning())
-	
+	get_tree().create_timer(10.0).timeout.connect(func (): TSP_Up_Center.enable_spawning())
